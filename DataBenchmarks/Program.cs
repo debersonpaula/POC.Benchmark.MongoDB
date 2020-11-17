99dabe04b8389b4f9f67fa4684867afb6ab6db11
Deberson de Paula <deberson.paula@gmail.com>
2020-11-17 08:51:37 -0300

    added Benchmark tests

---
+++ DataBenchmarks/Program.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using DataApi.Repository;
+
+namespace DataBenchmarks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<TestWithCollections>();
+            BenchmarkRunner.Run<TestWithDocuments>();
+        }
+    }
+
+    [MarkdownExporter, HtmlExporter]
+    [MinColumn, MaxColumn]
+    [MemoryDiagnoser]
+    public class TestBase
+    {
+        protected IPersonRepository _repo;
+
+        [Benchmark]
+        [Arguments(1)]
+        [Arguments(5)]
+        [Arguments(10)]
+        [Arguments(50)]
+        public void RunQuery(int qty)
+        {
+            for (var i = 1; i <= qty; i++)
+            {
+                _repo.GetPersonById(i).Wait();
+            }
+        }
+    }
+
+    public class TestWithCollections : TestBase
+    {
+        public TestWithCollections()
+        {
+            _repo = new PersonCollectionRepository();
+        }
+    }
+
+    public class TestWithDocuments : TestBase
+    {
+        public TestWithDocuments()
+        {
+            _repo = new PersonDocumentRepository();
+        }
+    }
+}
