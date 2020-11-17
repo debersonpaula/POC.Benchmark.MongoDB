76c209cafea2ab9c6c012fb5a552d6b394061e91
Deberson de Paula <deberson.paula@gmail.com>
2020-11-17 08:50:20 -0300

    added DataModels

---
+++ DataModels/OrderModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DataModels
+{
+    public class OrderModel
+    {
+        public int OrderId { get; set; }
+        public string ProductName { get; set; }
+        public string Price { get; set; }
+    }
+}
---
+++ DataModels/PersonModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataModels
+{
+    public class PersonModel
+    {
+        public Guid Id { get; set; }
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobArea { get; set; }
+        public string JobDescriptor { get; set; }
+        public string JobTitle { get; set; }
+        public string JobType { get; set; }
+        public IList<OrderModel> Orders { get; set; }
+    }
+}
